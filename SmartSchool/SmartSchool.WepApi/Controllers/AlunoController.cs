87a04948670bdc9d1a62288e0a017f0c149a4bf3
Rafaelferreiraalves <rafael.ferreira.alves5@gmail.com>
2020-05-27 18:29:02 -0300

    Implementacao controller aluno v1

--- SmartSchool/SmartSchool.WepApi/Controllers/AlunoController.cs
+++ SmartSchool/SmartSchool.WepApi/Controllers/AlunoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WepApi.Data;
 using SmartSchool.WepApi.Models;
 
 namespace SmartSchool.WepApi.Controllers
@@ -12,6 +13,11 @@ namespace SmartSchool.WepApi.Controllers
     [ApiController]
     public class AlunoController : ControllerBase
     {
+        private readonly DataContext context;
+        public AlunoController(DataContext context)
+        {
+            this.context = context;
+        }
         public List<Aluno> Alunos = new List<Aluno>()
         {
             new Aluno(){ Id =1,Nome="Marcos",Telefone="12355"},
@@ -24,16 +30,16 @@ namespace SmartSchool.WepApi.Controllers
         [HttpGet]
         public IEnumerable<Aluno> Get()
         {
-            return Alunos;
+            return context.Alunos;
         }
 
         // GET: api/Aluno/5
         [HttpGet("{id:int}")]
         public IActionResult Get(int id)
         {
-            Aluno aluno = (from alunoSelected in Alunos
-                          where alunoSelected.Id == id
-                          select alunoSelected).FirstOrDefault();
+            Aluno aluno = (from alunoSelected in context.Alunos
+                           where alunoSelected.Id == id
+                           select alunoSelected).FirstOrDefault();
             if (aluno != null)
                 return Ok(aluno);
             else
@@ -42,9 +48,9 @@ namespace SmartSchool.WepApi.Controllers
         [HttpGet("{name}")]
         public IActionResult Get(string name)
         {
-            var aluno = (from alunoSelected in Alunos
-                           where alunoSelected.Nome.Contains(name)
-                           select alunoSelected);
+            var aluno = (from alunoSelected in context.Alunos
+                         where alunoSelected.Nome.Contains(name)
+                         select alunoSelected);
             if (aluno != null)
                 return Ok(aluno);
             else
@@ -53,20 +59,48 @@ namespace SmartSchool.WepApi.Controllers
 
         // POST: api/Aluno
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Aluno value)
         {
+            try
+            {
+                context.Alunos.Add(value);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+
+            }
         }
 
         // PUT: api/Aluno/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Aluno value)
         {
+          
+            if (id == 0 )
+            {
+                return BadRequest();
+            }
+            context.Update(value);
+            context.SaveChanges();
+
+            return Ok();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var aluno = context.Alunos.Where(x => x.Id == id).FirstOrDefault();
+            if (id == 0 || aluno == null)
+            {
+                return BadRequest();
+            }
+
+            context.Alunos.Remove(aluno);
+            context.SaveChanges();
+            return Ok();
         }
     }
 }
---
+++ SmartSchool/SmartSchool.WepApi/Migrations/20200527003046_init.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartSchool.WepApi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    Sobrenome = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplinas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    ProfessorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplinas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Disciplinas_Professor_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlunoDisciplina",
+                columns: table => new
+                {
+                    AlunoId = table.Column<int>(nullable: false),
+                    DisciplinaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlunoDisciplina", x => new { x.AlunoId, x.DisciplinaId });
+                    table.ForeignKey(
+                        name: "FK_AlunoDisciplina_Alunos_AlunoId",
+                        column: x => x.AlunoId,
+                        principalTable: "Alunos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlunoDisciplina_Disciplinas_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "Disciplinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlunoDisciplina_DisciplinaId",
+                table: "AlunoDisciplina",
+                column: "DisciplinaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplinas_ProfessorId",
+                table: "Disciplinas",
+                column: "ProfessorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlunoDisciplina");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Disciplinas");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+        }
+    }
+}
