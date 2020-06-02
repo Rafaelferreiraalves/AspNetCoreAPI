559d2c686e36fe82360f6a58734759641fdd06ea
Rafaelferreiraalves <rafael.ferreira.alves5@gmail.com>
2020-06-01 21:59:14 -0300

    Repositorio primeira parte de aluno

--- SmartSchool/SmartSchool.WepApi/Data/IRepository.cs
+++ SmartSchool/SmartSchool.WepApi/Data/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartSchool.WepApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,23 @@ namespace SmartSchool.WepApi.Data
         void Update<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
         bool SaveChanges();
+
+        //Alunos
+        Aluno[] GetAllAlunos(bool includeDisciplinas=false);
+
+        Aluno[] GetAllByDisciplinaId(bool includeDisciplinas=false);
+        Aluno[] GetAlunoById(int id, bool includeDisciplinas=false);
+
+        //Professores
+
+        Professor[] GetAllProfessores();
+
+        Professor[] GetAllProfessorByDisciplina();
+        Professor[] GetAllProfessoresById();
+
+
+
+
+
     }
 }
--- SmartSchool/SmartSchool.WepApi/Data/Repository.cs
+++ SmartSchool/SmartSchool.WepApi/Data/Repository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.WepApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,6 +34,62 @@ namespace SmartSchool.WepApi.Data
             return context.SaveChanges() > 0 ;
         }
 
-     
+        public Aluno[] GetAllAlunos(bool includeDisciplinas = false)
+        {
+            IQueryable<Aluno> query = context.Alunos;
+            if (includeDisciplinas)
+            {
+                query = query.Include(aluno => aluno.AlunoDisciplina)
+                    .ThenInclude(disciplina => disciplina.Disciplina).ThenInclude(d => d.Professor);
+            }          
+
+            query = query.AsNoTracking().OrderBy(aluno => aluno.Id);
+
+            return query.ToArray();
+        }
+
+        public Aluno[] GetAllByDisciplinaId(bool includeDisciplinas = false)
+        {
+            IQueryable<Aluno> query = context.Alunos;
+            if (includeDisciplinas)
+            {
+                query = query.Include(aluno => aluno.AlunoDisciplina)
+                    .ThenInclude(disciplina => disciplina.Disciplina).ThenInclude(d => d.Professor);
+            }
+
+            query = query.AsNoTracking();
+
+            return query.ToArray();
+        }
+
+        public Aluno[] GetAlunoById(int id, bool includeDisciplinas = false)
+        {
+            IQueryable<Aluno> query = context.Alunos;
+            if (includeDisciplinas)
+            {
+                query = query.Include(aluno => aluno.AlunoDisciplina)
+                    .ThenInclude(disciplina => disciplina.Disciplina).ThenInclude(d => d.Professor);
+            }
+
+            query = query.AsNoTracking().Where(aluno => aluno.Id == id);
+
+            return query.ToArray();
+
+        }
+
+        public Professor[] GetAllProfessores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Professor[] GetAllProfessorByDisciplina()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Professor[] GetAllProfessoresById()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
